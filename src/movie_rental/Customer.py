e3cade54cb74a23d2a0c06fd75ea563ca1029111
ravik-karn-tw <60911433+ravik-karn-tw@users.noreply.github.com>
2022-10-14 15:36:06 +0530

    Ravi | Split loop refactoring for frequent renter point

--- src/movie_rental/Customer.py
+++ src/movie_rental/Customer.py
@@ -34,16 +34,17 @@ class Customer:
                     this_amount += 2
                     if each.get_days_rented() > 3:
                         this_amount += (each.get_days_rented() - 3) * 1.5
+            # show figure for this rental
+            result += "\t" + each.get_movie().get_title() + "\t" + \
+                      str(this_amount) + "\n"
+            total_amount += this_amount
+        for each in self.rentals:
             # add frequent renter points
             frequent_renter_points += 1
             # add bonus for a two day new release rental
             if (each.get_movie().get_price_code() == Movie.NEW_RELEASE) and \
                     (each.get_days_rented() > 1):
                 frequent_renter_points += 1
-            # show figure for this rental
-            result += "\t" + each.get_movie().get_title() + "\t" + \
-                      str(this_amount) + "\n"
-            total_amount += this_amount
         # add footer lines result
         result += "Amount owed is " + str(total_amount) + "\n"
         result += "You earned " + str(frequent_renter_points) + \
