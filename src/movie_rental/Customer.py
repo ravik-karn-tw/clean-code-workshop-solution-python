ff5c0adc56cc08e09dc232b1392f68a61ba0afda
ravik-karn-tw <60911433+ravik-karn-tw@users.noreply.github.com>
2022-10-15 14:59:18 +0530

    Ravi | Add statement class
    - Follow same steps as html statement

--- src/movie_rental/Customer.py
+++ src/movie_rental/Customer.py
@@ -1,4 +1,5 @@
 from src.movie_rental.HTMLStatement import HTMLStatement
+from src.movie_rental.Statement import Statement
 from src.movie_rental.Movie import Movie
 from src.movie_rental.Rental import Rental
 
@@ -18,29 +19,12 @@ class Customer:
         return self.name
 
     def statement(self) -> str:
-        return self.__header() + self.__body() + self.__footer()
-
-    def __header(self) -> str:
-        return "Rental Record for " + self.name + "\n"
-
-    def __body(self) -> str:
-        return self.__body_formatter(lambda rental: "\t" + rental.get_movie().get_title() + "\t" + \
-                                                    str(rental.amount_for()) + "\n")
-
-    def __footer(self) -> str:
-        return "Amount owed is " + str(self.__total_amount()) + "\n" + "You earned " + str(
-            self.__frequent_renter_points()) + \
-               " frequent renter points"
+        return Statement().generate_statement(self.name, self.rentals, self.__total_amount(),
+                                              self.__frequent_renter_points())
 
     def html_statement(self) -> str:
         return HTMLStatement().generate(self.name, self.rentals, self.__total_amount(),
                                         self.__frequent_renter_points())
-
-    def __body_formatter(self, formatter) -> str:
-        result: str = ""
-        for rental in self.rentals:
-            result += formatter(rental)
-        return result
 
     def __total_amount(self) -> float:
         total_amount: float = 0.0
---
+++ src/movie_rental/Statement.py
@@ -0,0 +1,22 @@
+class Statement:
+    def generate_statement(self, customer_name, rentals, total_amount, frequent_renter_points):
+        return self.__header(customer_name) + self.__body(rentals) + self.__footer(total_amount,
+                                                                                   frequent_renter_points)
+
+    def __header(self, customer_name) -> str:
+        return "Rental Record for " + customer_name + "\n"
+
+    def __body(self, rentals) -> str:
+        return self.__body_formatter(lambda rental: "\t" + rental.get_movie().get_title() + "\t" + \
+                                                    str(rental.amount_for()) + "\n", rentals)
+
+    def __footer(self, total_amount, frequent_renter_points) -> str:
+        return "Amount owed is " + str(total_amount) + "\n" + "You earned " + str(
+            frequent_renter_points) + \
+               " frequent renter points"
+
+    def __body_formatter(self, formatter, rentals) -> str:
+        result: str = ""
+        for rental in rentals:
+            result += formatter(rental)
+        return result
