b46891ba1a4fc04e55459d8b43ca278ed7665861
ravik-karn-tw <60911433+ravik-karn-tw@users.noreply.github.com>
2022-10-15 17:10:15 +0530

    Ravi | Rename variable

--- src/movie_rental/ChildrenPriceCode.py
+++ src/movie_rental/ChildrenPriceCode.py
@@ -1,6 +1,6 @@
 class ChildrenPriceCode:
     def amount(self, days_rented):
-        this_amount = 2
+        amount = 2
         if days_rented > 3:
-            this_amount += (days_rented - 3) * 1.5
-        return this_amount
+            amount += (days_rented - 3) * 1.5
+        return amount
--- src/movie_rental/RegularPriceCode.py
+++ src/movie_rental/RegularPriceCode.py
@@ -1,6 +1,6 @@
 class RegularPriceCode:
     def amount(self, days_rented):
-        this_amount: float = 2
+        amount: float = 2
         if days_rented > 2:
-            this_amount += (days_rented - 2) * 1.5
-        return this_amount
+            amount += (days_rented - 2) * 1.5
+        return amount
